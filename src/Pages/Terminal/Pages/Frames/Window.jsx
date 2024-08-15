3129a08bede14840abfaac63892acae0a76f73b8
Gabriel-H-Silva <gabrielhosws4@gmail.com>
2024-08-14 21:15:15 -0300

    Update

--- src/Pages/Terminal/Pages/Frames/Window.jsx
+++ src/Pages/Terminal/Pages/Frames/Window.jsx
@@ -1,3 +1,4 @@
+import { Button, Grid } from '@mui/material';
 import React from 'react';
 import Draggable from 'react-draggable';
 
@@ -6,7 +7,15 @@ function Window({ children, pLargura, pAltura }) {
     <Draggable handle=".frameTop">
       <div style={{ width: pLargura, height: pAltura, padding: '10px' }}>
         <div className='frameTop'>
-          
+          <Grid container>
+          <Grid item xs={6}>
+            nome
+          </Grid>
+          <Grid item xs={6} display={'flex'}>
+            <Button variant="outlined">-</Button>
+            <Button variant="outlined">X</Button>
+          </Grid>
+        </Grid>
         </div>
         <div className='frame'>
           {children}
